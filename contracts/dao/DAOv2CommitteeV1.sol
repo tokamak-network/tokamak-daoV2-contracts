abc1862d1bee4268e1e28eeb0c1eb93fbc0b2ec4
HyukSang <zzooppii@gmail.com>
2023-07-12 19:45:37 +0900

    update V1

--- contracts/dao/DAOv2CommitteeV1.sol
+++ contracts/dao/DAOv2CommitteeV1.sol
@@ -30,5 +30,83 @@ contract DAOv2CommitteeV1 is
     BaseProxyStorageV2,
     StorageStateCommitteeV2
 {
+    using SafeMath for uint256;
+    using LibAgenda for *;
 
+    modifier onlyOwner() {
+        require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "DAO: NA");
+        _;
+    }
+
+    modifier validMemberIndex(uint256 _index) {
+        require(_index < maxMember, "DAO: VI");
+        _;
+    }
+
+    modifier nonZero(address _addr) {
+        require(_addr != address(0), "DAO: ZA");
+        _;
+    }
+
+    /// @notice Set SeigManager contract address
+    /// @param _seigManager New SeigManager contract address
+    function setSeigManager(address _seigManager) external onlyOwner nonZero(_seigManager) {
+        seigManager = ISeigManager(_seigManager);
+    }
+
+    /// @notice Set SeigManager contract address on candidate contracts
+    /// @param _candidateContracts Candidate contracts to be set
+    /// @param _seigManager New SeigManager contract address
+    function setCandidatesSeigManager(
+        address[] calldata _candidateContracts,
+        address _seigManager
+    )
+        external
+        onlyOwner
+        nonZero(_seigManager)
+    {
+        for (uint256 i = 0; i < _candidateContracts.length; i++) {
+            ICandidate(_candidateContracts[i]).setSeigManager(_seigManager);
+        }
+    }
+
+    /// @notice Set DAOCommitteeProxy contract address on candidate contracts
+    /// @param _candidateContracts Candidate contracts to be set
+    /// @param _committee New DAOCommitteeProxy contract address
+    function setCandidatesCommittee(
+        address[] calldata _candidateContracts,
+        address _committee
+    )
+        external
+        onlyOwner
+        nonZero(_committee)
+    {
+        for (uint256 i = 0; i < _candidateContracts.length; i++) {
+            ICandidate(_candidateContracts[i]).setCommittee(_committee);
+        }
+    }
+
+    /// @notice Set DAOVault contract address
+    /// @param _daoVault New DAOVault contract address
+    function setDaoVault(address _daoVault) external onlyOwner nonZero(_daoVault) {
+        daoVault = IDAOVault(_daoVault);
+    }
+
+    /// @notice Set Layer2Registry contract address
+    /// @param _layer2Registry New Layer2Registry contract address
+    function setLayer2Registry(address _layer2Registry) external onlyOwner nonZero(_layer2Registry) {
+        layer2Registry = ILayer2Registry(_layer2Registry);
+    }
+
+    /// @notice Set CandidateFactory contract address
+    /// @param _candidateFactory New CandidateFactory contract address
+    function setCandidateFactory(address _candidateFactory) external onlyOwner nonZero(_candidateFactory) {
+        candidateFactory = ICandidateFactory(_candidateFactory);
+    }
+
+    /// @notice Set DAOAgendaManager contract address
+    /// @param _agendaManager New DAOAgendaManager contract address
+    function setAgendaManager(address _agendaManager) external onlyOwner nonZero(_agendaManager) {
+        agendaManager = IDAOAgendaManager(_agendaManager);
+    }
 }
--- contracts/dao/DAOv2CommitteeV2.sol
+++ contracts/dao/DAOv2CommitteeV2.sol
@@ -133,55 +133,55 @@ contract DAOv2CommitteeV2 is
     //////////////////////////////////////////////////////////////////////
     // V1 Owner
 
-    /// @notice Set SeigManager contract address
-    /// @param _seigManager New SeigManager contract address
-    function setSeigManager(address _seigManager) external onlyOwner nonZero(_seigManager) {
-        seigManager = ISeigManager(_seigManager);
-    }
+    // /// @notice Set SeigManager contract address
+    // /// @param _seigManager New SeigManager contract address
+    // function setSeigManager(address _seigManager) external onlyOwner nonZero(_seigManager) {
+    //     seigManager = ISeigManager(_seigManager);
+    // }
 
-    /// @notice Set SeigManager contract address on candidate contracts
-    /// @param _candidateContracts Candidate contracts to be set
-    /// @param _seigManager New SeigManager contract address
-    function setCandidatesSeigManager(
-        address[] calldata _candidateContracts,
-        address _seigManager
-    )
-        external
-        onlyOwner
-        nonZero(_seigManager)
-    {
-        for (uint256 i = 0; i < _candidateContracts.length; i++) {
-            ICandidate(_candidateContracts[i]).setSeigManager(_seigManager);
-        }
-    }
+    // /// @notice Set SeigManager contract address on candidate contracts
+    // /// @param _candidateContracts Candidate contracts to be set
+    // /// @param _seigManager New SeigManager contract address
+    // function setCandidatesSeigManager(
+    //     address[] calldata _candidateContracts,
+    //     address _seigManager
+    // )
+    //     external
+    //     onlyOwner
+    //     nonZero(_seigManager)
+    // {
+    //     for (uint256 i = 0; i < _candidateContracts.length; i++) {
+    //         ICandidate(_candidateContracts[i]).setSeigManager(_seigManager);
+    //     }
+    // }
 
-    /// @notice Set DAOCommitteeProxy contract address on candidate contracts
-    /// @param _candidateContracts Candidate contracts to be set
-    /// @param _committee New DAOCommitteeProxy contract address
-    function setCandidatesCommittee(
-        address[] calldata _candidateContracts,
-        address _committee
-    )
-        external
-        onlyOwner
-        nonZero(_committee)
-    {
-        for (uint256 i = 0; i < _candidateContracts.length; i++) {
-            ICandidate(_candidateContracts[i]).setCommittee(_committee);
-        }
-    }
+    // /// @notice Set DAOCommitteeProxy contract address on candidate contracts
+    // /// @param _candidateContracts Candidate contracts to be set
+    // /// @param _committee New DAOCommitteeProxy contract address
+    // function setCandidatesCommittee(
+    //     address[] calldata _candidateContracts,
+    //     address _committee
+    // )
+    //     external
+    //     onlyOwner
+    //     nonZero(_committee)
+    // {
+    //     for (uint256 i = 0; i < _candidateContracts.length; i++) {
+    //         ICandidate(_candidateContracts[i]).setCommittee(_committee);
+    //     }
+    // }
 
-    /// @notice Set Layer2Registry contract address
-    /// @param _layer2Registry New Layer2Registry contract address
-    function setLayer2Registry(address _layer2Registry) external onlyOwner nonZero(_layer2Registry) {
-        layer2Registry = ILayer2Registry(_layer2Registry);
-    }
+    // /// @notice Set Layer2Registry contract address
+    // /// @param _layer2Registry New Layer2Registry contract address
+    // function setLayer2Registry(address _layer2Registry) external onlyOwner nonZero(_layer2Registry) {
+    //     layer2Registry = ILayer2Registry(_layer2Registry);
+    // }
 
-    /// @notice Set CandidateFactory contract address
-    /// @param _candidateFactory New CandidateFactory contract address
-    function setCandidateFactory(address _candidateFactory) external onlyOwner nonZero(_candidateFactory) {
-        candidateFactory = ICandidateFactory(_candidateFactory);
-    }
+    // /// @notice Set CandidateFactory contract address
+    // /// @param _candidateFactory New CandidateFactory contract address
+    // function setCandidateFactory(address _candidateFactory) external onlyOwner nonZero(_candidateFactory) {
+    //     candidateFactory = ICandidateFactory(_candidateFactory);
+    // }
 
     //////////////////////////////////////////////////////////////////////
     // V2 Owner
@@ -192,11 +192,11 @@ contract DAOv2CommitteeV2 is
         seigManagerV2 = ISeigManagerV2(_seigManagerV2);
     }
 
-    /// @notice Set DAOVault contract address
-    /// @param _daoVault New DAOVault contract address
-    function setDaoVault(address _daoVault) external onlyOwner nonZero(_daoVault) {
-        daoVault = IDAOVault(_daoVault);
-    }
+    // /// @notice Set DAOVault contract address
+    // /// @param _daoVault New DAOVault contract address
+    // function setDaoVault(address _daoVault) external onlyOwner nonZero(_daoVault) {
+    //     daoVault = IDAOVault(_daoVault);
+    // }
 
     /// @notice Set Layer2Manager contract address
     /// @param _layer2Manager New Layer2Manager contract address
@@ -205,11 +205,11 @@ contract DAOv2CommitteeV2 is
         // layer2Manager = ILayer2Manager(_layer2Manager);
     }
 
-    /// @notice Set DAOAgendaManager contract address
-    /// @param _agendaManager New DAOAgendaManager contract address
-    function setAgendaManager(address _agendaManager) external onlyOwner nonZero(_agendaManager) {
-        agendaManager = IDAOAgendaManager(_agendaManager);
-    }
+    // /// @notice Set DAOAgendaManager contract address
+    // /// @param _agendaManager New DAOAgendaManager contract address
+    // function setAgendaManager(address _agendaManager) external onlyOwner nonZero(_agendaManager) {
+    //     agendaManager = IDAOAgendaManager(_agendaManager);
+    // }
 
     function setCandidates(address _candidate) external onlyOwner nonZero(_candidate) {
         candidate = _candidate;
